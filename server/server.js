940b48047ee86708d060020864cf9f2650703fc3
Aseem Regmi <aseem.regmi22@gmail.com>
2018-09-10 18:42:04 +0545

    Emit new message on create Message

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  console.log('New User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('createMessage', message => {
+    console.log('New Message', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+});
+
+server.listen(port, () => {
   console.log(`Listening in PORT ${port}`);
 });
